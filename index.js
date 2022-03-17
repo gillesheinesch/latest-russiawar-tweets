763bf20be4dad656d85b782e36e2aeaa39b4675f
Gilles Heinesch <heineschgilles@gmail.com>
2022-03-17 22:36:32 +0100

    fixes

--- index.js
+++ index.js
@@ -14,52 +14,50 @@ console.log(`Latest russia vs ukraine tweets started at ${moment().format('DD/MM
 
 // Trigger every day at 20h05 pm
 cron.schedule('05 * * * *', async () => {
-let tweetIdToRetweet;
-// Search for tweets with the query "ukraine"
-client.get('search/tweets', {
-  q: 'ukraine',
-  include_entities:true,
-  filter:("links"||"videos"||"images")
-}, function (error, tweets, response) {
-  // Loop through all 15 last tweeted Tweets containing the term "ukraine"
-  // If a tweet with more than 300 likes has been found, save the id to a pre-defined variable "tweetIdToRetweet"
-  for (let i = 0; i < tweets.statuses.length; i += 1) {
-    if (tweets.statuses[i].retweeted_status) {
-      if (tweets.statuses[i].retweeted_status.favorite_count > 300) {
-        tweetIdToRetweet = tweets.statuses[i].retweeted_status.id_str;
-      }
-    } else {
-      if (tweets.statuses[i].favorite_count > 300) {
-        tweetIdToRetweet = tweets.statuses[i].id_str;
-      }
-    }
-  }
-
-  console.log(tweetIdToRetweet)
-
-  let tweetedTweets = [];
-  fs.readFile(`./retweetedTweets.json`, (err, data) => { // get the data from the file
-    if (data != '') {
-      tweetedTweets = JSON.parse(data);
-    }
-
-    console.log(tweetedTweets.tweets)
-
-    if (!tweetedTweets.tweets.includes(tweetIdToRetweet)) {
-      client.post('statuses/retweet/' + tweetIdToRetweet, function (error, tweet, response) {
-        if (!error) {
-          console.log(tweet);
+  let tweetIdToRetweet;
+  // Search for tweets with the query "ukraine"
+  client.get('search/tweets', {
+    q: 'ukraine',
+    include_entities: true,
+    filter: ("links" || "videos" || "images")
+  }, function (error, tweets, response) {
+    // Loop through all 15 last tweeted Tweets containing the term "ukraine"
+    // If a tweet with more than 300 likes has been found, save the id to a pre-defined variable "tweetIdToRetweet"
+    for (let i = 0; i < tweets.statuses.length; i += 1) {
+      if (tweets.statuses[i].retweeted_status) {
+        if (tweets.statuses[i].retweeted_status.favorite_count > 300) {
+          tweetIdToRetweet = tweets.statuses[i].retweeted_status.id_str;
         }
-      });
-
-      tweetedTweets.tweets.push(tweetIdToRetweet);
-
-      const jsonFormat = JSON.stringify(tweetedTweets)
-
-      fs.writeFile(`./retweetedTweets.json`, jsonFormat, (err) => {
-        console.log(err);
-      });
+      } else {
+        if (tweets.statuses[i].favorite_count > 300) {
+          tweetIdToRetweet = tweets.statuses[i].id_str;
+        }
+      }
     }
-  });
 
+    let tweetedTweets = [];
+    fs.readFile(`./retweetedTweets.json`, (err, data) => { // get the data from the file
+      if (data != '') {
+        tweetedTweets = JSON.parse(data);
+      }
+
+      if (!tweetedTweets.tweets.includes(tweetIdToRetweet)) {
+        client.post('statuses/retweet/' + tweetIdToRetweet, function (error, tweet, response) {
+          if (!error) {
+            console.log(tweet);
+          }
+        });
+
+        tweetedTweets.tweets.push(tweetIdToRetweet);
+
+        const jsonFormat = JSON.stringify(tweetedTweets)
+
+        fs.writeFile(`./retweetedTweets.json`, jsonFormat, (err) => {
+          console.log(err);
+        });
+
+        console.log(`Posted the following tweet: ` + tweetIdToRetweet)
+      }
+    });
+  })
 });
